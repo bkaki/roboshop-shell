eb6137ce36916dad5964e851cd66e89a22f8dfb4
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 04:28:52 +0530

    Adding Print statements

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ yum install mysql-community-server -y
 
 echo -e "\e[36m>>>>>>>>> start MySQl  <<<<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 
 echo -e "\e[36m>>>>>>>>> Restart MySQl password  <<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
