d7f90bad416e6e6aebc0759692677459adc6c116
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 04:26:13 +0530

    Adding Print statements

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>>>>> Disable MySQl 8 version <<<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>> copy MySQl repo file  <<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>> Install MySQl  <<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>> start MySQl  <<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[36m>>>>>>>>> Restart MySQl password  <<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
