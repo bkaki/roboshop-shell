8a685fd7c96d5fd8fa13e86c563de8905acfa69e
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 05:02:58 +0530

    Adding Print statements

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,8 +1,16 @@
+echo -e "\e[36m>>>>>>>>> Install erlang repos <<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-yum install erlang -y
+
+echo -e "\e[36m>>>>>>>>> Install Rabbitmq repos  <<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-yum install rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>> Install erlang & Rabbitmq  <<<<<<<<<<\e[0m"
+yum install erlang rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>> Start rabbitmq services  <<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
+
+echo -e "\e[36m>>>>>>>>> Add application user in Rabbitmq  <<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
