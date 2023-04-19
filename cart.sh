63a493360a465b5c113700b284b21d166df78d25
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 04:18:55 +0530

    Adding Print statements

--- cart.sh
+++ cart.sh
@@ -1,13 +1,29 @@
+echo -e "\e[36m>>>>>>>>> Install NodeJs repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>> Create Application cart <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create Application directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Create App content <<<<<<<<<<\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>> unzip App content <<<<<<<<<<\e[0m"
 unzip /tmp/cart.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>> Install npm dependencies <<<<<<<<<<\e[0m"
 npm install
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>> Copy cart systemd file <<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
