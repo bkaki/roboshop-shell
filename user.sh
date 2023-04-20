d8af9fb3b5e9f0a8fefc4df22be76d5e68a8bace
bkaki <bhaskarkakii77@gmail.com>
2023-04-21 04:11:07 +0530

    Adding Print statements

--- user.sh
+++ user.sh
@@ -1,6 +1,11 @@
-script_path=$(dirname $0)
-source ${script_path}/common.sh
+script=$(realpath "$0")
 
+realpath $0
+
+exit
+
+script_path=$(dirname "$SCRIPT")
+source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>> Install NodeJs repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
