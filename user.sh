d8116bba5b95f4016369822642427d2076d8314c
bkaki <bhaskarkakii77@gmail.com>
2023-04-21 14:27:26 +0530

    Adding Print statements

--- user.sh
+++ user.sh
@@ -25,7 +25,7 @@ echo -e "\e[36m>>>>>>>>> Install npm dependencies <<<<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>> Copy user systemd file <<<<<<<<<<\e[0m"
-cp ${script_path}/user.service /etc/systemd/system/user.service
+cp $script_path/user.service /etc/systemd/system/user.service
 
 echo -e "\e[36m>>>>>>>>> Start user Service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
@@ -33,7 +33,7 @@ systemctl enable user
 systemctl restart user
 
 echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
