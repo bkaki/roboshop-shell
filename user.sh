9a6591a75f908ca4e4a61b78519ed6346a7c0f2e
bkaki <bhaskarkakii77@gmail.com>
2023-04-21 14:23:38 +0530

    Adding Print statements

--- user.sh
+++ user.sh
@@ -1,9 +1,6 @@
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-echo $script_path
-exit
-
 echo -e "\e[36m>>>>>>>>> Install NodeJs repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
@@ -28,7 +25,7 @@ echo -e "\e[36m>>>>>>>>> Install npm dependencies <<<<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>> Copy user systemd file <<<<<<<<<<\e[0m"
-cp home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp ${script_path}/user.service /etc/systemd/system/user.service
 
 echo -e "\e[36m>>>>>>>>> Start user Service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
@@ -36,7 +33,7 @@ systemctl enable user
 systemctl restart user
 
 echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<<<\e[0m"
-cp home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
