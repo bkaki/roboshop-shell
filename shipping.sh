2e87c3e820f58b2279175929a4026824908291a0
bkaki <bhaskarkakii77@gmail.com>
2023-04-23 14:05:12 +0530

    Adding Print statements

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-mysql_root_passwd= $1
+mysql_root_password=$1
 
 echo -e "\e[36m>>>>>>>>> Install Maven <<<<<<<<<<\e[0m"
 yum install maven -y
@@ -32,7 +32,7 @@ echo -e "\e[36m>>>>>>>>> setup systemd services <<<<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 
 echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<<<\e[0m"
-mysql -h mysql-dev.bhaskar77.online -uroot -p{myaql_root_password} < /app/schema/shipping.sql
+mysql -h mysql-dev.bhaskar77.online -uroot -p${myaql_root_password} < /app/schema/shipping.sql
 
 echo -e "\e[36m>>>>>>>>> restart system services <<<<<<<<<<\e[0m"
 systemctl daemon-reload
