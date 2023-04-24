36e40c0d89c6e6854c6f99f9beb62bbc638886d5
bkaki <bhaskarkakii77@gmail.com>
2023-04-24 22:16:17 +0530

    Adding Print statements

--- common.sh
+++ common.sh
@@ -6,18 +6,28 @@ print_head() {
   func_print_head "\e[36m>>>>>>>>> $1 <<<<<<<<<<\e[0m"
 
 }
-
+ func_status_check() {
+ if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit
+  fi
+  }
 func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
 
     func_print_head "Copy MongoDB repo"
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    func_stat_check $?
 
    func_print_head "Install MongoDB Client"
    yum install mongodb-org-shell -y
+   func_stat_check $?
 
    func_print_head "load schema"
    mongo --host mongodb-dev.bhaskar77.online </app/schema/${component}.js
+   func_stat_check $?
 
   fi
 
@@ -25,9 +35,11 @@ func_schema_setup() {
 
      func_print_head "Install MySql client"
      yum install mysql -y
+     func_stat_check $?
 
       func_print_head "Load schema"
       mysql -h mysql-dev.bhaskar77.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+      func_stat_check $?
 
     fi
 }
@@ -35,17 +47,21 @@ fucn_app_prereq() {
 
  func_print_head "Create application user"
   useradd ${app_user}
+  func_stat_check $?
 
   func_print_head "Create application directory"
   rm -rf /app
   mkdir /app
+  func_stat_check $?
 
   func_print_head "Download app content"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
+  func_stat_check $?
 
   func_print_head "unzip app content"
   unzip /tmp/${component}.zip
+  func_stat_check $?
 
 }
 
@@ -54,6 +70,7 @@ func_systemd_setup() {
     func_print_head "setup systemd services"
 
     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+    func_stat_check $?
 
     func_print_head "restart system services"
 
@@ -66,26 +83,20 @@ func_systemd_setup() {
 func_nodejs() {
   func_print_head "Install NodeJs repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_stat_check $?
 
   func_print_head "Install NodeJs"
   yum install nodejs -y
 
-  func_print_head "Create Application cart"
-  useradd ${app_user}
+  func_stat_check $?
 
-  func_print_head "Create Application directory"
-  rm -rf /app
-  mkdir /app
-
-  func_print_head "Create App content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-  func_print_head "unzip App content"
-  unzip /tmp/${component}.zip
+  func_app_prereq
 
   func_print_head "Install npm dependencies"
+
   npm install
+
+  func_stat_check $?
 
   Func_schema_setup
 
@@ -99,10 +110,13 @@ func_java() {
   func_print_head "Install Maven"
   yum install maven -y
 
+  func_stat_check $?
+
   func_app_prereq
 
   func_print_head "Download maven dependencies"
   mvn clean package
+  func_stat_check $?
   mv target/${component}-1.0.jar ${component}.jar
 
   Func_schema_setup
