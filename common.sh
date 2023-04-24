1594f7bc455c8dff73a33ae2376f39df5e225439
bkaki <bhaskarkakii77@gmail.com>
2023-04-25 04:19:45 +0530

    Adding Print statements

--- common.sh
+++ common.sh
@@ -14,7 +14,7 @@ print_head() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
-    echo "Refer the log file /tmp/roboshop.log for more information
+    echo "Refer the log file /tmp/roboshop.log for more information"
     exit
   fi
   }
