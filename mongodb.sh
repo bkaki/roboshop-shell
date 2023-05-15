4b8263a932d38e752487697126eebd474b3c8bd8
bkaki <bhaskarkakii77@gmail.com>
2023-05-16 05:04:25 +0530

    terraform

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
-
-# Edit the file and replace 127.0.0.1 to 0.0.0.0
-
 systemctl restart mongod
