7a1bab3be9905540e8c5f3bda4e919527e5de6c9
bkaki <bhaskarkakii77@gmail.com>
2023-05-17 03:35:49 +0530

    terraform

--- redis.sh
+++ redis.sh
@@ -1,5 +1,5 @@
 
-echo -e "\e[36m>>>>>>>>>>>> Configure Repo file <<<<<<<<<<<<\e[om"
+echo -e "\e[36m>>>>>>>>>>>> Install Redis Repo file <<<<<<<<<<<<\e[om"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 
