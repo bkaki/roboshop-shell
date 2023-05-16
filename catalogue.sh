797837743d556d952dc32c025ebd7d554e1dc390
bkaki <bhaskarkakii77@gmail.com>
2023-05-16 05:49:31 +0530

    terraform

--- catalogue.sh
+++ catalogue.sh
@@ -5,35 +5,35 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo -e "\e[36m>>>>>>> Add user <<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>> Add Application user <<<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "e\[36m>>>>>>>>> Add app directory <<<<<<<<<<\e[0m"
+echo -e "e\[36m>>>>>>>>> create app directory <<<<<<<<<<\e[0m"
 mkdir /app
 
-echo -e "\e[36m>>>>>>>>> Download application content <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Download app content <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo -e "\e[36m>>>>>>>>>>> Unzip app directory <<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>> Unzip app content <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 
-echo -e "\e[36m>>>>>>>>> Install dependencies <<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Install NodJS dependencies <<<<<<<<<\e[0m"
 npm install
 
-echo -e "\e[36m>>>>>>>> Copy service file <<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> Copy Catalogue SystemD file <<<<<<<<<<<\e[0m"
 cp calalogue.service /etc/systemd/system/catalogue.service
 
-echo -e "\e[36m>>>>>>>> System service file <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>> Start Catalogue service  <<<<<<<<\e[0m"
 
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
-echo -e "\e[36m>>>>>>>>>> Download Repo file <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Copy MongoDB Repo  <<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[36m>>>>>>>> Install Mongodb >>>>>>\e[om"
+echo -e "\e[36m>>>>>>>> Install Mongodb client >>>>>>\e[om"
 
 yum install mongodb-org-shell -y
 
