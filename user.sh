6dfec008c23bd708516d389901304713318942ea
bkaki <bhaskarkakii77@gmail.com>
2023-05-17 03:34:59 +0530

    terraform

--- user.sh
+++ user.sh
@@ -1,27 +1,46 @@
+echo -e "\e[36m>>>>>>>>> Configuring NodeJs Repos <<<<<<<<<<\e[0m"
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<\e[0m"
 
 yum install nodejs -y
 
+echo -e "\e[36m>>>>>>> Add Application user <<<<<<<<<\e[0m"
+
+
 useradd roboshop
 
+echo -e "e\[36m>>>>>>>>> create app directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
 unzip /tmp/user.zip
+
+echo -e "\e[36m>>>>>>>>> Install NodJS dependencies <<<<<<<<<\e[0m"
 
 cd /app
 npm install
 
-cp user.service /etc/systemd/system/user.service
+echo -e "\e[36m>>>>>>>> Copy User SystemD file <<<<<<<<<<<\e[0m"
+
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>> Start Catalogue service  <<<<<<<<\e[0m"
 
 systemctl daemon-reload
-
 systemctl enable user
-systemctl start user
+systemctl restart user
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>> Copy MongoDB Repo  <<<<<<<<\e[0m"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>> Install Mongodb client >>>>>>\e[om"
 
 yum install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<\e[0m"
 
 mongo --host mongodb-dev.bhaskar77.online </app/schema/user.js
