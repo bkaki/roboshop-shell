a1979c705f9392825fc7787479f878179d46b744
bkaki <bhaskarkakii77@gmail.com>
2023-05-17 05:28:22 +0530

    terraform

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,25 @@
+
+echo -e "\e[36m>>>>>>>>> Install Nginx <<<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[36m>>>>>>>>> Copy Roboshop Conf <<<<<<<<<\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>> Remove default web content <<<<<<<<<\e[0m"
+
 rm -rf /usr/share/nginx/html/*
+
+
+echo -e "\e[36m>>>>>>>>> Download frontend content <<<<<<<<<\e[0m"
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m>>>>>>>>> Extract frontend content <<<<<<<<<\e[0m"
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>>> Systemd service file <<<<<<<<<\e[0m"
+
 systemctl enable nginx
 systemctl restart nginx
