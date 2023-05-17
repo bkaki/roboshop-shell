88dcacf12c3c6e61848ba0e685e6ae9ea31ee3f8
bkaki <bhaskarkakii77@gmail.com>
2023-05-17 05:46:28 +0530

    terraform

--- redis.sh
+++ redis.sh
@@ -1,11 +1,13 @@
 
 echo -e "\e[36m>>>>>>>>>>>> Install Redis Repo file <<<<<<<<<<<<\e[om"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
+
 
 echo -e "\e[36m>>>>>>>>Install redis <<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
+echo -e "\e[36m>>>>>>>>>> update redis listen address <<<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 
 echo -e "\e[36m>>>>>>>>>> Systemd service file <<<<<<<<<<\e[0m"
