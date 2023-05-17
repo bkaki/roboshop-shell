9866e46fe97c9f890066d8a6aa20ef8a97e50541
bkaki <bhaskarkakii77@gmail.com>
2023-05-18 01:34:09 +0530

    terraform

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,13 +2,16 @@ echo -e "\e[36m>>>>>>>>>> Setup erlang repo <<<<<<<<<<\e[0m"
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
+echo -e "\e[36m>>>>>>>>>> SInstall erlang <<<<<<<<<<\e[0m"
+yum install erlang -y
+
 echo -e "\e[36m>>>>>>>>>> Setup rabbitmq repo <<<<<<<<<<\e[0m"
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
-echo -e "\e[36m>>>>>>>>>> Install erlang & Rabbitmq <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Install Rabbitmq <<<<<<<<<<\e[0m"
 
-yum install erlang rabbitmq-server -y
+yum install rabbitmq-server -y
 
 echo -e "\e[36m>>>>>>>>>> Start system files <<<<<<<<<<\e[0m"
 
@@ -16,7 +19,6 @@ systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
 
 echo -e "\e[36m>>>>>>>>>> Add application User in Rabbitmq <<<<<<<<<<\e[0m"
-
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
