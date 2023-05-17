29ceed4874b9a3ff75c8bf305dae9c0ac8d6245e
bkaki <bhaskarkakii77@gmail.com>
2023-05-18 02:17:02 +0530

    terraform

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,18 +1,18 @@
 echo -e "\e[36m>>>>>>>>>> Setup erlang repo <<<<<<<<<<\e[0m"
-
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
-echo -e "\e[36m>>>>>>>>>> SInstall erlang <<<<<<<<<<\e[0m"
+
+echo -e "\e[36m>>>>>>>>>> Install erlang <<<<<<<<<<\e[0m"
 yum install erlang -y
 
-echo -e "\e[36m>>>>>>>>>> Setup rabbitmq repo <<<<<<<<<<\e[0m"
 
+echo -e "\e[36m>>>>>>>>>> Setup rabbitmq repo <<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
 
 echo -e "\e[36m>>>>>>>>>> Install Rabbitmq <<<<<<<<<<\e[0m"
 
 yum install rabbitmq-server -y
-
 echo -e "\e[36m>>>>>>>>>> Start system files <<<<<<<<<<\e[0m"
 
 systemctl enable rabbitmq-server
