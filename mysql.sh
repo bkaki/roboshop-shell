c153a67f745d1275e2763e88240e87a9a8f91a64
bkaki <bhaskarkakii77@gmail.com>
2023-05-18 00:56:34 +0530

    terraform

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,20 @@
+
+echo -e "\e[36m>>>>>>>>> Disable mysql 8 version <<<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>> copy mysql repo <<<<<<<<<<\e[0m"
+cp home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>> Install mysql  <<<<<<<<<<\e[0m"
+
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>> Start Mysql  <<<<<<<<<<\e[0m"
+
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[36m>>>>>>>>> reset Mysql password <<<<<<<<<<\e[0m"
+
 mysql_secure_installation --set-root-pass RoboShop@1
 
