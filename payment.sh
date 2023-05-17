f8675bf5b18d1eef1cca12fb380018651a791a93
bkaki <bhaskarkakii77@gmail.com>
2023-05-18 01:53:52 +0530

    terraform

--- payment.sh
+++ payment.sh
@@ -8,6 +8,7 @@ echo -e "\e[36m>>>>>>>>> Add app user <<<<<<<<<<\e[0m"
 useradd roboshop
 
 echo -e "\e[36m>>>>>>>>> Add app directory <<<<<<<<<<\e[0m"
+
 rm -rf /app
 mkdir /app
 
@@ -15,16 +16,17 @@ echo -e "\e[36m>>>>>>>>> Download app content <<<<<<<<<<\e[0m"
 
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 
+
 echo -e "\e[36m>>>>>>>>> Extract app content <<<<<<<<<<\e[0m"
+
 cd /app
 unzip /tmp/payment.zip
-
 
 echo -e "\e[36m>>>>>>>>> Install Python dependencies <<<<<<<<<<\e[0m"
 
 pip3.6 install -r requirements.txt
 
-echo -e "\e[36m>>>>>>>>> Copy Systemd service files <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Copy Systemd service  <<<<<<<<<<\e[0m"
 
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 
@@ -32,4 +34,4 @@ echo -e "\e[36m>>>>>>>>> start payment service <<<<<<<<<<\e[0m"
 
 systemctl daemon-reload
 systemctl enable payment
-systemctl restart payment
+systemctl start payment
