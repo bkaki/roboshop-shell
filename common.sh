8964ba717b7674a910d28849c2742001f2f9c08b
bkaki <bhaskarkakii77@gmail.com>
2023-05-20 20:23:18 +0530

    terraform

--- common.sh
+++ common.sh
@@ -2,34 +2,38 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+print_head () {
+  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<<<\e[0m"
+}
+
 func_nodejs() {
-echo -e "\e[36m>>>>>>>>> Configuring NodeJs Repos <<<<<<<<<<\e[0m"
+print_head Download NodeJS repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<\e[0m"
+print_head Install NodeJS
 yum install nodejs -y
 
-echo -e "\e[36m>>>>>>> Add Application user <<<<<<<<<\e[0m"
+print_head Add Application user
 useradd ${app_user}
 
-echo -e "e\[36m>>>>>>>>> create app directory <<<<<<<<<<\e[0m"
+create app directory
 rm -rf /app
 mkdir /app
 
-echo -e "\e[36m>>>>>>>>> Download app content <<<<<<<<\e[0m"
+print_head Download app content
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 
-echo -e "\e[36m>>>>>>>>>>> Unzip app content <<<<<<<<<\e[0m"
+print_head Unzip app content
 cd /app
 unzip /tmp/${component}.zip
 
-echo -e "\e[36m>>>>>>>>> Install NodJS dependencies <<<<<<<<<\e[0m"
+print_head Install NodJS dependencies
 npm install
 
-echo -e "\e[36m>>>>>>>> Copy Catalogue SystemD file <<<<<<<<<<<\e[0m"
+print_head Copy Catalogue SystemD file
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-echo -e "\e[36m>>>>>>>> Start Catalogue service  <<<<<<<<\e[0m"
+print_head Start Catalogue service
 
 systemctl daemon-reload
 systemctl enable ${component}
